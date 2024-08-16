e5d661ad5ac58dc374c174f7041c8ed8cd1bb2e0
Kurtko10 <kurtkoaf@gmail.com>
2024-08-16 21:58:01 +0200

    Mejora de botones para dispositivos moviles

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import reactLogo from "../../assets/react.svg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faProjectDiagram, faTools, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import profileImage from "../../assets/Angel.jpg";
 import Button from 'react-bootstrap/Button';
 import "./Home.css";
@@ -9,25 +10,20 @@ const Home = () => {
 
   const renderContent = () => {
     switch (activeSection) {
-      case 'about':
-        return <p>Sobre mí: Información sobre mí.</p>;
       case 'projects':
         return <p>Proyectos: Aquí están mis proyectos.</p>;
-      case 'skills':
-        return <p>Habilidades: Estas son mis habilidades.</p>;
+      case 'why':
+        return <p>¿Por qué?: Creo que debo ser elegido para los grupos de trabajo por....</p>;
       case 'contact':
         return <p>Contacto: Aquí puedes contactarme.</p>;
       default:
         return (
           <>
-            {/* <img src="path-to-your-image.jpg" alt="Principal" className="main-image" /> */}
-             <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-        <img src={profileImage} className="name-image" alt="React logo" />
-      </a>
-      <h1>Ángel Fernández Bravo</h1>
-      <h3>Proyecto inicial AuJ</h3>
-      <h6>Proyecto para optar a formar parte de los grupos de trabajo AuJ.</h6>
-      <p>Creo que es una gran oportunidad para poder formar parte de proyectos reales y así mejorar mi perfil en el sector</p>
+            <img src={profileImage} className="name-image" alt="Profile" />
+            <h1>Ángel Fernández Bravo</h1>
+            <h3>Proyecto inicial AuJ</h3>
+            <h6>Proyecto para optar a formar parte de los grupos de trabajo AuJ.</h6>
+            <p>Creo que es una gran oportunidad para poder formar parte de proyectos reales y así mejorar mi perfil en el sector</p>
             <p>Este es el texto principal debajo de la foto.</p>
           </>
         );
@@ -38,11 +34,34 @@ const Home = () => {
     <div className="main-content">
       {renderContent()}
       <div className="buttons">
-        <Button variant="outline-info"onClick={() => setActiveSection('/')} >Sobre mí</Button>
-        <Button variant="outline-info" onClick={() => setActiveSection('/')}>Sobre mí</Button>
-        <Button variant="outline-info" onClick={() => setActiveSection('projects')}>Proyectos</Button>
-        <Button onClick={() => setActiveSection('skills')}>Habilidades</Button>
-        <Button onClick={() => setActiveSection('contact')}>Contacto</Button>
+        <Button
+          variant="outline-info"
+          onClick={() => setActiveSection('home')}
+          className={`icon-button ${activeSection === '/' ? 'active' : ''}`}
+        >
+          <FontAwesomeIcon icon={faUser} /> <span className="button-text">Sobre mí</span>
+        </Button>
+        <Button
+          variant="outline-info"
+          onClick={() => setActiveSection('projects')}
+          className={`icon-button ${activeSection === 'projects' ? 'active' : ''}`}
+        >
+          <FontAwesomeIcon icon={faProjectDiagram} /> <span className="button-text">Proyectos</span>
+        </Button>
+        <Button
+          variant="outline-info"
+          onClick={() => setActiveSection('why')}
+          className={`icon-button ${activeSection === 'why' ? 'active' : ''}`}
+        >
+          <FontAwesomeIcon icon={faTools} /> <span className="button-text">¿Por qué?</span>
+        </Button>
+        <Button
+          variant="outline-info"
+          onClick={() => setActiveSection('contact')}
+          className={`icon-button ${activeSection === 'contact' ? 'active' : ''}`}
+        >
+          <FontAwesomeIcon icon={faEnvelope} /> <span className="button-text">Contacto</span>
+        </Button>
       </div>
     </div>
   );
